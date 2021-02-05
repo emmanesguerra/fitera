e90b43c7d44ec68e4a7b5e61fbf8f3224d1950c3
emmanesguerra <emman.esguerra2013@gmail.com>
2021-02-05 10:22:25 +0800

    fix displaying of property details with img modal

--- property-listing/resources/views/property.blade.php
+++ property-listing/resources/views/property.blade.php
@@ -17,16 +17,37 @@
                     <li class="breadcrumb-item active" aria-current="page">Roof Deck</li>
                 </ol>
             </nav>
-            
-            
-            <div class='col-6'>
-                <img src='{{ asset() }}'>
+
+
+            <div class='col-6 mt-2 mb-5'>
+                <img src='{{ asset('img/cubao.jpg') }}' class="img-fluid" style='cursor: pointer;' onclick="display(this)" />
+                <div class='thumb'>
+                    <img src='{{ asset('img/cubao.jpg') }}' class="" onclick="display(this)" />
+                    <img src='{{ asset('img/edsa-station-pasay.jpg') }}' class="" onclick="display(this)" />
+                    <img src='{{ asset('img/edsa-traffic-ber-months-situation-1566985698.jpg') }}' class="" onclick="display(this)" />
+                    <img src='{{ asset('img/the-historic-edsa.jpeg') }}' class="" onclick="display(this)" />
+                </div>
             </div>
-            <div class='col-6'>
+            <div class='col-6 pdetails px-5 mt-2 mb-5'>
+                <h1 class="title">Roof deck</h1>
+                <span class="loc">North Edsa Station</span>
+                <span class="size">Southbound Size: 50ft x 30ft</span>
+                <span class="details">Vicinity: Farmer's Market, Alimal, Araneta Coliseum, Gaateway Mall</span>
+                <span class="details">INFO: Positioned on ighly visible, heavy traffic locations succh as expressways and major roadways.</span>
+                <span class="details">ADDRESS: North Avenue corner EDSA, Barangay Bagong Pag-asa, Quezon City</span>
+
+                <img src='{{ asset('img/north-edsa-map.jpg') }}' class='img-thumbnail' width="300" onclick="display(this)"/>
+
+                <button class='btn btn-primary'>BOOK NOW!</button>
             </div>
-            
+
         </div>
     </div>
+</div>
+
+<div id="myModal" class="modal">
+    <img src='{{ asset('img/close.png') }}' id='clx' class='clx'>
+    <img class="modal-content" id="img01">       
 </div>
 
 <div class="topsearch">
@@ -51,3 +72,20 @@
 </div>
 
 @include('nav.footer')
+
+
+<script>
+    var listcounter = {{ $count }};
+    
+    function display (element) {
+        var modal = $("#myModal");
+        var modalImg = $("#img01");
+        
+        $("#myModal").css('display', 'block');
+        $("#img01").attr('src', $(element).attr('src'));
+        
+        $('#clx').on('click', function () {
+            $("#myModal").css('display', 'none');
+        });
+    }
+</script>
